7ab7e2adf3d7fb947679bb02297b963544e24b76
darkz70 <d91313250@gmail.com>
2026-04-28 05:25:00 +0200

    Update gradlew.bat for improved Java detection

--- gradlew.bat
+++ gradlew.bat
@@ -1,2 +1,42 @@
-@echo off
-REM Gradle wrapper launcher
+@ECHO OFF
+
+SET DIRNAME=%~dp0
+IF "%DIRNAME%"=="" SET DIRNAME=.
+SET APP_HOME=%DIRNAME%
+
+SET CLASSPATH=%APP_HOME%\gradle\wrapper\gradle-wrapper.jar
+
+IF DEFINED JAVA_HOME GOTO findJavaFromJavaHome
+SET JAVA_EXE=java.exe
+%JAVA_EXE% -version >NUL 2>&1
+IF "%ERRORLEVEL%" == "0" GOTO execute
+
+ECHO.
+ECHO ERROR: JAVA_HOME is not set and no 'java' command could be found in your PATH.
+ECHO.
+GOTO fail
+
+:findJavaFromJavaHome
+SET JAVA_HOME=%JAVA_HOME:"=%
+SET JAVA_EXE=%JAVA_HOME%\bin\java.exe
+
+IF EXIST "%JAVA_EXE%" GOTO execute
+
+ECHO.
+ECHO ERROR: JAVA_HOME is set to an invalid directory: %JAVA_HOME%
+ECHO.
+GOTO fail
+
+:execute
+"%JAVA_EXE%" -classpath "%CLASSPATH%" org.gradle.wrapper.GradleWrapperMain %*
+
+:end
+IF "%ERRORLEVEL%"=="0" GOTO mainEnd
+
+:fail
+EXIT /B 1
+
+:mainEnd
+IF "%OS%"=="Windows_NT" ENDLOCAL
+
+:omega
